1195d7c8d4608843bb6a6e7670159ac91a47414f
zoniha <awaawapurin@gmail.com>
2021-08-16 16:22:28 +0900

    Initial Commit

---
+++ SampleMapAppWithSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleMapAppWithSwift
+//
+//  Created by zoniha on 2021/08/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SampleMapAppWithSwift/SampleMapAppWithSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleMapAppWithSwiftApp.swift
+//  SampleMapAppWithSwift
+//
+//  Created by zoniha on 2021/08/16.
+//
+
+import SwiftUI
+
+@main
+struct SampleMapAppWithSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
