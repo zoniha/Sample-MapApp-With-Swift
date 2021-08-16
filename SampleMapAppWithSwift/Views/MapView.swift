25fd1d821d6651c20592198ccf02050c4e364115
zoniha <awaawapurin@gmail.com>
2021-08-16 23:32:38 +0900

    ✨ feat: Add CLGeocoder

--- SampleMapAppWithSwift/Views/MapView.swift
+++ SampleMapAppWithSwift/Views/MapView.swift
@@ -1,13 +1,39 @@
 import SwiftUI
+import MapKit
 
-struct MapView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+struct MapView: UIViewRepresentable {
+	var searchKey: String
+
+	func makeUIView(context: Context) -> MKMapView {
+		MKMapView()
+	}
+
+	func updateUIView(_ uiView: MKMapView, context: Context) {
+		let geocoder = CLGeocoder()
+
+		geocoder.geocodeAddressString(searchKey, completionHandler: { (placemarks, error) in
+			if let unwrapPlacemarks = placemarks,
+			   let firstPlacemark = unwrapPlacemarks.first,
+			   let location = firstPlacemark.location {
+				let targetCoordinate = location.coordinate
+				let pin = MKPointAnnotation()
+
+				pin.coordinate = targetCoordinate
+				pin.title = searchKey
+
+				uiView.addAnnotation(pin)
+				uiView.region = MKCoordinateRegion(
+					center: targetCoordinate,
+					latitudinalMeters: 500.0,
+					longitudinalMeters: 500.0
+				)
+			}
+		})
+	}
 }
 
 struct MapView_Previews: PreviewProvider {
-    static var previews: some View {
-        MapView()
-    }
+	static var previews: some View {
+		MapView(searchKey: "東京ドーム")
+	}
 }
